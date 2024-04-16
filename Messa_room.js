b72e339580199ecef70b91c64ad39ac1aeac51dd
Harshit2012 <105143145+Harshit2012@users.noreply.github.com>
2024-04-16 17:58:48 +0530

    Update Messa_room.js
    Loading Effect Added

--- Messa_room.js
+++ Messa_room.js
@@ -7,34 +7,42 @@ const firebaseConfig = {
     messagingSenderId: "304263375919",
     appId: "1:304263375919:web:6cb4b76deed0b4e2222ada",
     measurementId: "G-60J28LHKHG"
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-     Room_names = childKey;
-    //Start code
-    console.log("Room Name - " + Room_names);
-    row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>"+Room_names+"</div><hr>"
-    document.getElementById("output").innerHTML += row;
-    //End code
-    });});}
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+function getData() {
+    firebase.database().ref("/").on('value', function (snapshot) {
+        document.getElementById("output").innerHTML = ""; snapshot.forEach(function (childSnapshot) {
+            childKey = childSnapshot.key;
+            Room_names = childKey;
+            //Start code
+            console.log("Room Name - " + Room_names);
+            row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoomName(this.id)'>" + Room_names + "</div><hr>"
+            document.getElementById("output").innerHTML += row;
+            //End code
+        });
+    });
+}
 getData();
-function addRoom(){
+function addRoom() {
     room_name = document.getElementById("Room_Name").value;
     firebase.database().ref("/").child(room_name).update({
-          purpose : "adding room name"
+        purpose: "adding room name"
     });
     localStorage.setItem("room_name", room_name);
     window.location = "Messa_page.html";
 }
-function redirectToRoomName(name){
+function redirectToRoomName(name) {
     console.log(name);
     localStorage.setItem("room_name", name);
     window.location = "Messa_page.html";
 }
-function logout(){
+function logout() {
     localStorage.removeItem("user_name");
     localStorage.removeItem("room_name");
     window.location = "index.html";
 }
+$(window).load(function () {
+    $(".se-pre-con").fadeOut("slow");;
+});
